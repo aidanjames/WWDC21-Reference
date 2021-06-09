34f83e525c43f1f944399a335dcc823fc67de240
AidanJP <aidanpendlebury@gmail.com>
2021-06-09 18:40:38 +0100

    Initial Commit

---
+++ Shared/WWDC21_ReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC21_ReferenceApp.swift
+//  Shared
+//
+//  Created by Aidan Pendlebury on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WWDC21_ReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
