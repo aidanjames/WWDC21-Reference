812d9495abb84cfdf2a67a06edbd665e781678b1
AidanJP <aidanpendlebury@gmail.com>
2021-06-14 17:07:27 +0100

    Updating swipe action view to have an actual proper action.

--- Shared/SwipeActionsView.swift
+++ Shared/SwipeActionsView.swift
@@ -9,27 +9,49 @@ import SwiftUI
 
 struct SwipeActionsView: View {
     
-    @State private var tmsTeam = ["Aidan", "Ian", "Terry", "Wayne", "Jon", "Roo", "Cliff"]
+    @State private var members: [Member] = []
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(tmsTeam, id: \.self) { member in
-                    Text(member)
+                ForEach(members) { member in
+                    Text(member.name)
                         .swipeActions(allowsFullSwipe: false) {
                             Button(action: { print("This!") } ) {
                                 Text("Print")
                             }
-                            Button(role: .destructive, action: { print("That!") } ) {
+                            Button(role: .destructive, action: {
+                                if let index = members.firstIndex(where: { $0.id == member.id } ) {
+                                    members.remove(at: index)
+                                }
+                            } ) {
                                 Image(systemName: "trash")
                             }
                         }
                 }
                 .navigationTitle("Members")
             }
+            .task { members = Member.createMembers() }
         }
     }
 }
+
+
+struct Member: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    
+    static func createMembers() -> [Member] {
+        let team = ["Aidan", "Ian", "Terry", "Wayne", "Jon", "Roo", "Cliff"]
+        var returnTeam: [Member] = []
+        for person in team {
+            let newMember = Member(name: person)
+            returnTeam.append(newMember)
+        }
+        return returnTeam
+    }
+}
+
 
 struct SwipeActionsView_Previews: PreviewProvider {
     static var previews: some View {
