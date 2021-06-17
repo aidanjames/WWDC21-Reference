6d3e169369e838cc78cc27f22c449aa1cfbfd627
AidanJP <aidanpendlebury@gmail.com>
2021-06-17 18:22:49 +0100

    Added some processing to the ConfirmationDialogView.

--- Shared/ConfirmationDialogView.swift
+++ Shared/ConfirmationDialogView.swift
@@ -9,17 +9,21 @@ import SwiftUI
 
 struct ConfirmationDialogView: View {
     @State private var confirmationViewPresented = false
+    @State private var mood: String = ""
+    
+    let moods = ["😊", "☹️", "😐"]
     
     var body: some View {
-        Button {
-            confirmationViewPresented.toggle()
-        } label: {
-            Text("This is the button")
-        }
-        .confirmationDialog("Do you want to do this?", isPresented: $confirmationViewPresented, titleVisibility: .visible) {
-            Button("Let's do it", role: .destructive) {
-                print("Do something here")
+        VStack {
+            Button {
+                confirmationViewPresented.toggle()
+            } label: {
+                Text("Press to see your mood...")
             }
+            Text(mood).padding()
+                .confirmationDialog("Are you sure you want to do this?", isPresented: $confirmationViewPresented, titleVisibility: .visible) {
+                    Button("Let's do it", role: .destructive) { mood = moods.randomElement()! }
+                }
         }
     }
 }
